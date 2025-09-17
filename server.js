6fa08b43a0ce8d16a5fe9c45c948ff8ee2862423
ChaahatL <chaahatlokhande@gmail.com>
2025-09-17 12:17:28 +0530

    updated server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
 // Middleware
