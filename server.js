ca58fbb21de7b4379b9fe051301ea23cab1e4fb5
ChaahatL <chaahatlokhande@gmail.com>
2025-09-17 12:55:08 +0530

    updated server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
